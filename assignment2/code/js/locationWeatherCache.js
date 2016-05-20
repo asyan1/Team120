b6a25472772f87d9e84fb96d6334dc9f8e9737b5
ssud17 <ssud17@student.monash.edu.au>
2016-05-20 14:37:25 +1000

    Update locationWeatherCache.js

--- assignment2/code/js/locationWeatherCache.js
+++ assignment2/code/js/locationWeatherCache.js
@@ -1,138 +1,89 @@
+// Code for the Add Location page.
 
-// Returns a date in the format "YYYY-MM-DD".
-Date.prototype.simpleDateString = function() {
-    function pad(value)
-    {
-        return ("0" + value).slice(-2);
-    }
-
-    var dateString = this.getFullYear() + "-" + 
-            pad(this.getMonth() + 1, 2) + '-' + 
-            pad(this.getDate(), 2);
-    
-    return dateString;
-}
-
-// Date format required by forecast.io API.
-// We always represent a date with a time of midday,
-// so our choice of day isn't susceptible to time zone errors.
-Date.prototype.forecastDateString = function() {
-    return this.simpleDateString() + "T12:00:00";
-}
-
-
-// Code for LocationWeatherCache class and other shared code.
-
-// Prefix to use for Local Storage.  You may change this.
-var APP_PREFIX = "weatherApp";
-
-function LocationWeatherCache()
+function initMap() 
 {
-    // Private attributes:
-
-    var locations = [];
-    var callbacks = {};
-
-    // Public methods:
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: -35.9120467, lng: 145.1343136},
+        zoom: 16
+    });
+            
+    map.setCenter({lat: -37.9120467, lng: 145.1343136});
+                            
+    marker1 = new google.maps.Marker({
+        position: {lat: -37.9128781, lng: 145.1362585},
+        label: {text: "Sport" },   
+        map: map,
+        title: "Monash Sport"             
+    });
     
-    // Returns the number of locations stored in the cache.
-    //
-    this.length = function() {
-        return locations.length;
-        
-    };
-    
-    // Returns the location object for a given index.
-    // Indexes begin at zero.
-    //
-    this.locationAtIndex = function(index) {
-        return locations[index];
-        
-    };
+    // This example adds a search box to a map, using the Google Place Autocomplete
+// feature. People can enter geographical searches. The search box will return a
+// pick list containing a mix of places and predicted search terms.
 
-    // Given a latitude, longitude and nickname, this method saves a 
-    // new location into the cache.  It will have an empty 'forecasts'
-    // property.  Returns the index of the added location.
-    //
-    this.addLocation = function(latitude, longitude, nickname)
-    {
-        locations.push[newlocation]
-        var newlocation = { 
-            lat:lattitude,
-            long:longitude,
-            nick:nickname,
-            }
-        return locations.length;
+// This example requires the Places library. Include the libraries=places
+// parameter when you first load the API. For example:
+// <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
+
+function initAutocomplete() {
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: -33.8688, lng: 151.2195},
+    zoom: 13,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  });
+
+  // Create the search box and link it to the UI element.
+  var input = document.getElementById('pac-input');
+  var searchBox = new google.maps.places.SearchBox(input);
+  map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+  // Bias the SearchBox results towards current map's viewport.
+  map.addListener('bounds_changed', function() {
+    searchBox.setBounds(map.getBounds());
+  });
+
+  var markers = [];
+  // Listen for the event fired when the user selects a prediction and retrieve
+  // more details for that place.
+  searchBox.addListener('places_changed', function() {
+    var places = searchBox.getPlaces();
+
+    if (places.length == 0) {
+      return;
     }
 
-    // Removes the saved location at the given index.
-    // 
-    this.removeLocationAtIndex = function(index)
-    {
-       return locations.splice(index, 1);
-    }
+    // Clear out the old markers.
+    markers.forEach(function(marker) {
+      marker.setMap(null);
+    });
+    markers = [];
 
-    // This method is used by JSON.stringify() to serialise this class.
-    // Note that the callbacks attribute is only meaningful while there 
-    // are active web service requests and so doesn't need to be saved.
-    //
-    this.toJSON = function() {
-        var locationsasJSON = JSON.stringify(locations)
-        return locationsasJSON;
-    };
+    // For each place, get the icon, name and location.
+    var bounds = new google.maps.LatLngBounds();
+    places.forEach(function(place) {
+      var icon = {
+        url: place.icon,
+        size: new google.maps.Size(71, 71),
+        origin: new google.maps.Point(0, 0),
+        anchor: new google.maps.Point(17, 34),
+        scaledSize: new google.maps.Size(25, 25)
+      };
 
-    // Given a public-data-only version of the class (such as from
-    // local storage), this method will initialise the current
-    // instance to match that version.
-    //
-    this.initialiseFromPDO = function(locationWeatherCachePDO) {
-        
-        
-    };
+      // Create a marker for each place.
+      markers.push(new google.maps.Marker({
+        map: map,
+        icon: icon,
+        title: place.name,
+        position: place.geometry.location
+      }));
 
-    // Request weather for the location at the given index for the
-    // specified date.  'date' should be JavaScript Date instance.
-    //
-    // This method doesn't return anything, but rather calls the 
-    // callback function when the weather object is available. This
-    // might be immediately or after some indeterminate amount of time.
-    // The callback function should have two parameters.  The first
-    // will be the index of the location and the second will be the 
-    // weather object for that location.
-    // 
-    this.getWeatherAtIndexForDate = function(index, date, callback) {
-    };
-    
-    // This is a callback function passed to forecast.io API calls.
-    // This will be called via JSONP when the API call is loaded.
-    //
-    // This should invoke the recorded callback function for that
-    // weather request.
-    //
-    this.weatherResponse = function(response) {
-    };
-
-    // Private methods:
-    
-    // Given a latitude and longitude, this method looks through all
-    // the stored locations and returns the index of the location with
-    // matching latitude and longitude if one exists, otherwise it
-    // returns -1.
-    //
-    function indexForLocation(latitude, longitude)
-    {
-    }
-}
-
-// Restore the singleton locationWeatherCache from Local Storage.
-//
-function loadLocations()
-{
-}
-
-// Save the singleton locationWeatherCache to Local Storage.
-//
-function saveLocations()
-{
+      if (place.geometry.viewport) {
+        // Only geocodes have viewport.
+        bounds.union(place.geometry.viewport);
+      } else {
+        bounds.extend(place.geometry.location);
+      }
+    });
+    map.fitBounds(bounds);
+  });
 }
 
